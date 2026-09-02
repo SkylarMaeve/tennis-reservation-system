07cd41359620bbebcd422c471bdbe0f2dc2db49c
Nikol Otáhalová <536358@mail.muni.cz>
2026-09-02 15:54:13 +0200

    Add: Data Access Object Tests for Court and Reservation

--- inqool-application/src/main/java/skylarmaeve/inqoolapplication/TennisReservationSystem.java
+++ inqool-application/src/main/java/skylarmaeve/inqoolapplication/TennisReservationSystem.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InqoolApplication {
+public class TennisReservationSystem {
 
     public static void main(String[] args) {
-        SpringApplication.run(InqoolApplication.class, args);
+        SpringApplication.run(TennisReservationSystem.class, args);
     }
 
 }
